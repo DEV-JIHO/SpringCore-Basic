c783c05772fb11c9bced9a8175a3689d8a2c8579
Hello <wjdwlgh_34@naver.com>
2024-02-24 21:30:38 +0900

    add Lombok

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,6 +1,12 @@
 package hello.core;
 
 
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -12,5 +18,12 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )//AppConfig 클래스의 빈이 중복해서 등록되지 않게 하기위해서
 public class AutoAppConfig {
+
+/*
+    @Bean
+    OrderService orderService() {
+        return new OrderServiceImpl(memberRepository, discountPolicy);
+    }
+*/
 
 }
